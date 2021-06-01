2593b67885ba9cc016a9f6c3b2fdeedf6675fbf9
Alejandro Ramirez Aldariz <627538+janodev@users.noreply.github.com>
2021-06-01 13:24:17 +0200

    Prevent NSInvalidArgumentException serializing invalid JSON.
    Such exception is not translated as a Swift error, so it won’t be caught by do/try or avoided just writing try?.

--- sources/main/json/Data+JSON.swift
+++ sources/main/json/Data+JSON.swift
@@ -6,8 +6,13 @@ public extension Data
         try? JSONSerialization.jsonObject(with: self, options: [])
     }
     func toJSONString() -> String? {
-        guard let jsonObject = toJSON() else { return nil }
-        guard let data = try? JSONSerialization.data(withJSONObject: jsonObject, options: [.sortedKeys, .prettyPrinted]) else { return nil }
+        guard
+            let jsonObject = toJSON(),
+            JSONSerialization.isValidJSONObject(jsonObject),
+            let data = try? JSONSerialization.data(withJSONObject: jsonObject, options: [.sortedKeys, .prettyPrinted])
+        else {
+            return nil
+        }
         return String(data: data, encoding: String.Encoding.utf8)
     }
 }
